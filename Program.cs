95d426c270780f8c4dddfc8b3054b00410be744f
Jessica <jpanndora@gmail.com>
2025-10-03 01:07:11 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double b, h, area;
+Console.Write("Enter base: ");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter height: ");
+h = Convert.ToDouble(Console.ReadLine());
+area = (b * h) / 2;
+Console.WriteLine($"\nÁrea....: {area:N3}");
